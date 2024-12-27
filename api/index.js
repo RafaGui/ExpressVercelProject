d2bc75b571677dbc4288342b91e2cabf1da68c75
RafaGui <rafaelwilliansgi4@gmail.com>
2024-12-27 13:00:55 -0300

    infra: setting vercel project

--- api/index.js
+++ api/index.js
@@ -1,3 +1,22 @@
-const { app } = require("../index");
+const express = require('express');
+const path = require('path');
 
-module.exports = app;
+const app = express();
+
+// Middleware para servir arquivos estáticos
+app.use(express.static(path.join(__dirname, '../public')));
+
+// Exemplo de rota
+app.get('/', (req, res) => {
+  res.send('Olá, mundo!');
+});
+
+// Rota para o frontend
+app.get('/app', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+// Exportando a função que será chamada pelo Vercel
+module.exports = (req, res) => {
+  app(req, res);
+};
